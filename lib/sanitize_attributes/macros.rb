77f83704854b8dfe6d23f4657804fb7fb22b6b30
Dev Purkayastha <dev@forgreatjustice.net>
2011-07-13 21:17:13 -0700

    fix to allow providing options + callback for :before_validation instead

--- lib/sanitize_attributes/macros.rb
+++ lib/sanitize_attributes/macros.rb
@@ -2,12 +2,19 @@ module SanitizeAttributes
   module Macros
     # sanitize_attributes is used to define sanitizable attributes within a model definition.
     def sanitize_attributes(*args, &block)
-
+      if (args.last && args.last.is_a?(Hash))
+        options = args.pop
+      end
+      options ||= {}
       unless @sanitize_hook_already_defined
         include InstanceMethods
         extend ClassMethods
 
-        before_save :sanitize!
+        if options[:before_validation]
+          before_validation :sanitize!
+        else
+          before_save :sanitize!
+        end
 
         cattr_accessor :sanitizable_attribute_hash
         cattr_accessor :sanitization_block_array
