703f67ffb48fb658a8c7a3be218057e92cba7695
Dev Purkayastha <dev@forgreatjustice.net>
2011-11-26 00:28:15 -0500

    attempting Rails 2 compatability

--- lib/sanitize_attributes.rb
+++ lib/sanitize_attributes.rb
@@ -9,14 +9,18 @@ module SanitizeAttributes
     attr_accessor :default_sanitization_method    
     
     def hook!
-      ActiveSupport.on_load(:active_record) { extend SanitizeAttributes }
+      if ActiveSupport.respond_to?(:on_load)
+        ActiveSupport.on_load(:active_record) { extend SanitizeAttributes }
+      else # Rails 2 fallback
+        ActiveRecord::Base.class_eval { extend SanitizeAttributes }
+      end
     end
   end
   
   # Thrown when sanitize! is called without a defined sanitization method at the class or global level
   class NoSanitizationMethodDefined < StandardError ; end  
 
-  if defined?(Rails)
+  if defined?(Rails) && defined?(Rails::Railtie)
     class Railtie < Rails::Railtie
       initializer("sanitize_attirbutes") { SanitizeAttributes.hook! }
     end
